c6ffee4e70db65c335276a0371460e0af1d23575
20161104604 <a20161104604@ciec-imacs-imac.local>
2018-06-27 10:53:30 +0800

    Initial Commit

---
+++ zcp1/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  zcp1
+//
+//  Created by 20161104604 on 2018/6/27.
+//  Copyright © 2018年 20161104604. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
